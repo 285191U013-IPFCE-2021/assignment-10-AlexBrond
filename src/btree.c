72a7c71a493a67a03dd28a27e04c5c03dff07057
AlexBrond <alexander-111199@hotmail.com>
2021-11-15 20:40:16 +0100

    Assignment 10

--- src/btree.c
+++ src/btree.c
@@ -6,45 +6,160 @@
 #include<stdbool.h>
 
 
-struct tree_node * Insert (int x, struct tree_node *t)
+// C program to demonstrate
+// delete operation in binary
+// search tree
+#include <stdio.h>
+#include <stdlib.h>
+ 
+struct node {
+    int key;
+    struct node *left, *right;
+};
+ 
+// A utility function to create a new BST node
+struct node* newNode(int item)
 {
-  // Insert item x into the tree t
-
-  return NULL;
+    struct node* temp
+        = (struct node*)malloc(sizeof(struct node));
+    temp->key = item;
+    temp->left = temp->right = NULL;
+    return temp;
 }
-
-struct tree_node * Remove (int x, struct tree_node *t)
+ 
+// A utility function to do inorder traversal of BST
+void inorder(struct node* root)
 {
- // Remove one item from the tree t
-
-  return NULL;
+    if (root != NULL) {
+        inorder(root->left);
+        printf("%d ", root->key);
+        inorder(root->right);
+    }
 }
-
-
-int Contains (int x, struct tree_node *t)
+ 
+/* A utility function to
+   insert a new node with given key in
+ * BST */
+struct node* insert(struct node* node, int key)
 {
-
-  // Return true if the tree t contains item x. Return false otherwise.
-
-  return NULL;
-
+    /* If the tree is empty, return a new node */
+    if (node == NULL)
+        return newNode(key);
+ 
+    /* Otherwise, recur down the tree */
+    if (key < node->key)
+        node->left = insert(node->left, key);
+    else
+        node->right = insert(node->right, key);
+ 
+    /* return the (unchanged) node pointer */
+    return node;
 }
-
-
-struct tree_node * Initialize (struct tree_node *t)
+ 
+/* Given a non-empty binary search
+   tree, return the node
+   with minimum key value found in
+   that tree. Note that the
+   entire tree does not need to be searched. */
+struct node* minValueNode(struct node* node)
 {
-  // Create an empty tree
-  return NULL;
+    struct node* current = node;
+ 
+    /* loop down to find the leftmost leaf */
+    while (current && current->left != NULL)
+        current = current->left;
+ 
+    return current;
 }
-
-int Empty (struct tree_node *t)
+ 
+/* Given a binary search tree
+   and a key, this function
+   deletes the key and
+   returns the new root */
+struct node* deleteNode(struct node* root, int key)
 {
-  // Test if empty
-  return 0;
+    // base case
+    if (root == NULL)
+        return root;
+ 
+    // If the key to be deleted
+    // is smaller than the root's
+    // key, then it lies in left subtree
+    if (key < root->key)
+        root->left = deleteNode(root->left, key);
+ 
+    // If the key to be deleted
+    // is greater than the root's
+    // key, then it lies in right subtree
+    else if (key > root->key)
+        root->right = deleteNode(root->right, key);
+ 
+    // if key is same as root's key,
+    // then This is the node
+    // to be deleted
+    else {
+        // node with only one child or no child
+        if (root->left == NULL) {
+            struct node* temp = root->right;
+            free(root);
+            return temp;
+        }
+        else if (root->right == NULL) {
+            struct node* temp = root->left;
+            free(root);
+            return temp;
+        }
+ 
+        // node with two children:
+        // Get the inorder successor
+        // (smallest in the right subtree)
+        struct node* temp = minValueNode(root->right);
+ 
+        // Copy the inorder
+        // successor's content to this node
+        root->key = temp->key;
+ 
+        // Delete the inorder successor
+        root->right = deleteNode(root->right, temp->key);
+    }
+    return root;
 }
-
-int Full (struct tree_node *t)
+ 
+// Driver Code
+int main()
 {
-    // Test if full
+    /* Let us create following BST
+              50
+           /     \
+          30      70
+         /  \    /  \
+       20   40  60   80 */
+    struct node* root = NULL;
+    root = insert(root, 50);
+    root = insert(root, 30);
+    root = insert(root, 20);
+    root = insert(root, 40);
+    root = insert(root, 70);
+    root = insert(root, 60);
+    root = insert(root, 80);
+ 
+    printf("Inorder traversal of the given tree \n");
+    inorder(root);
+ 
+    printf("\nDelete 20\n");
+    root = deleteNode(root, 20);
+    printf("Inorder traversal of the modified tree \n");
+    inorder(root);
+ 
+    printf("\nDelete 30\n");
+    root = deleteNode(root, 30);
+    printf("Inorder traversal of the modified tree \n");
+    inorder(root);
+ 
+    printf("\nDelete 50\n");
+    root = deleteNode(root, 50);
+    printf("Inorder traversal of the modified tree \n");
+    inorder(root);
+ 
     return 0;
 }
--- src/linked_list.c
+++ src/linked_list.c
@@ -36,16 +36,122 @@ void free_list (node * p)
 /* print list to console */
 void print_list (node * p)
 {
-    // Add your code for exercise 1
-    // There is NO testcode for this
+  struct node {
+    int number;
+    struct node *next;
+};
+
+struct node *insert (struct node *head) {
+   // return new list pointer
+    int b = 0;
+    struct node *current;
+    printf("Insert: ");
+    if (1 != scanf("%d", &b) || b == -1)
+        return head;
+    current = malloc(sizeof(struct node));
+    if (current == NULL) {
+        printf("Bad malloc!\n");
+        exit (1);
+        }
+    current->number = b;
+    current->next = head;
+    return insert(current);
+}
+
+void display (struct node *list) {
+    if (list) {
+        printf("%d \t", list->number);
+        display(list->next);
+    }
+}
+
+void free_list (struct node *list) {
+    if (list) {
+        free_list(list->next);
+        free(list);
+    }
+}
+
+int main(void) {
+    struct node *head = NULL;
+    head = insert (head);
+
+    printf("List is: ");
+    display(head);
+    printf("\n");
+
+    free_list (head);
+    return 0;
+
 }
 
 int sum_squares (node * p)
 {
-    // Add your code for excercise 2
-    // You can find the tests in tests.cpp
-    return -1;
+    typedef struct node
+{
+   int value;
+   struct node * next;
+} node;
+
+/* terminal node at the end of the list */
+static node SENTINEL_node = { 0, 0 };
+    
+/* functions to create lists */
+node *make_node (int v, node * q)
+{
+  node *p = malloc (sizeof (node));
+  p->value = v;
+
+  if (q == &SENTINEL_node)
+    p->next = &SENTINEL_node;
+  else
+    p->next = q;
+  return p;
 }
+
+/* squares a given integer */
+int square (int x)
+{
+  return x * x;
+}
+
+int sum_squares (node * p)
+{
+
+    if (p == &SENTINEL_node) // if list is empty
+      return 0;
+    else
+      return (square(p -> value) + sum_squares(p -> next));
+}
+
+/* free all nodes in the list p */
+void free_list (node * p)
+{
+  node *q = p;
+  while (q != &SENTINEL_node)
+    {
+      node *t = q->next;
+      free (q);
+      q = t;
+    }
+}
+int main (void)
+{
+    int sum;
+      node *ns = NULL;
+        ns = make_node (1,
+            make_node (2,
+                   make_node (3,
+                          make_node (4,
+                              make_node (5,
+                                    &SENTINEL_node)))));
+
+    sum = sum_squares (ns); /* sum should equal 55 */
+    printf("\n%d", sum);
+    free_list(ns);
+    return 0;
+}
+
 
 node *map (node * p, int (*f) (int))
 {
